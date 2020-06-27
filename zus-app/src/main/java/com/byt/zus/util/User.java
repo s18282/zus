2675400936db5881c14205c41a82f245a033d136
Marcin Kruk <kruk.marcin21@gmail.com>
2020-06-27 18:50:51 +0200

    Added functionality to find currently logged in user username

---
+++ zus-app/src/main/java/com/byt/zus/util/User.java
@@ -0,0 +1,22 @@
+package com.byt.zus.util;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User {
+
+    public static String getCurrentUserUsername() {
+
+        Object principal = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
+    }
+
+}
