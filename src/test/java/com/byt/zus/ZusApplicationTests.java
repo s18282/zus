22e522e092209fcc7a1132b28d2c3cf3d25bc0e2
Marcin Kruk <kruk.marcin21@gmail.com>
2020-06-27 14:13:53 +0200

    init

---
+++ src/test/java/com/byt/zus/ZusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byt.zus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
